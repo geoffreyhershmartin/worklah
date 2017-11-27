678706760e9ee6ed790a93e146d8c31a32332caf
Geoffrey Martin <Geoffrey@Geoffreys-MacBook-Pro.local>
2017-11-27 12:09:16 +0800

    added object sending functionality

--- src/client/Message.java
+++ src/client/Message.java
@@ -9,16 +9,19 @@ public class Message implements Serializable{
 	    // if no match is found, then an InvalidClassException
 	    
 	    
-	    public String type, sender, content, recipient;
+	    public String type, sender, content;
+	    public Group group;
 	    
-	    public Message(String type, String sender, String content, String recipient) {
+	    public Message(String type, String sender, String content, Group _group) {
 	        this.type = type;
-	        this.sender = sender; this.content = content; this.recipient = recipient;
+	        this.sender = sender;
+	        this.content = content;
+	        this.group = _group;
 	    }
 	    
 	    @Override
 	    public String toString(){
-	        return "{type='"+type+"', sender='"+sender+"', content='"+content+"', recipient='"+recipient+"'}";
+	        return "{type='"+type+"', sender='"+sender+"', content='"+content+"'}";
 	    }
 	}
 
--- src/gui/LoginScreenController.java
+++ src/gui/LoginScreenController.java
@@ -62,7 +62,7 @@ public class LoginScreenController implements Initializable {
 		ChatController controller = fxmlLoader.<ChatController>getController();
 		int port = 8080;
         String server = "127.0.0.1";
-        client = new ChatClient(server, port, controller);
+        client = new ChatClient(server, port, controller, userID);
         client.start();
 		controller.setClient(client);
 		Scene scene = new Scene(root); 
