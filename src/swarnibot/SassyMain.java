ff9c4238cdb0241766bea5c45a104095a0fde17e
Swarnima Sircar <32955011+ssircar@users.noreply.github.com>
2017-11-29 10:36:09 +0800

    Update and rename changes to SassyMain

--- src/swarnibot/SassyMain.java
+++ src/swarnibot/SassyMain.java
@@ -3,12 +3,14 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package swarnibot;
+package sassybot;
 
 /**
  *
  * @author student
  */
-public class SwarniMain {
+public class SassyMain {
+    
+    
     
 }
